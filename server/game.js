bf82df6139339a85bfd65b5aeca13c7ab6b17180
Leo LAFON <leo.lafon@epitech.eu>
2016-11-01 13:33:26 +0100

    Linted game.js with semi colon

--- server/game.js
+++ server/game.js
@@ -5,66 +5,66 @@ var util = require('util');
 function Game () {
 	EventEmitter.call(this);
 
-	var self = this
-	self.map = Array(19).fill(0).map(() => Array(19).fill(0))
-	self.players = {1: false, 2: false}
-	self.turn = 1
+	var self = this;
+	self.map = Array(19).fill(0).map(() => Array(19).fill(0));
+	self.players = {1: false, 2: false};
+	self.turn = 1;
 
 	self.connectPlayer = function connectPlayer() {
 		if (!self.players[1]) {
-			self.players[1] = true
-			return 1
+			self.players[1] = true;
+			return 1;
 		}
 		else if (!self.players[2]) {
-			self.players[2] = true
-			self.emit('ready')
-			return 2
+			self.players[2] = true;
+			self.emit('ready');
+			return 2;
 		}
 		else
 			return 0
-	}
+	};
 
 	self.getConnectedPlayers = function getConnectedPlayers() {
 		return {
 			player1: self.players[1],
 			player2: self.players[2]
-		}
-	}
+		};
+	};
 
 	self.isReady = function isReady() {
-		return (self.players[1] === true && self.players[2] === true)
-	}
+		return (self.players[1] === true && self.players[2] === true);
+	};
 
 	self.getMap = function getMap() {
-		return {map: self.map}
-	}
+		return {map: self.map};
+	};
 
 	self.getTurn = function getTurn() {
-		return {current: (self.turn % 2 == 0) + 1, total: self.turn}
-	}
+		return {current: (self.turn % 2 == 0) + 1, total: self.turn};
+	};
 
 	// async, because leo's functions could use some async functionalities
 	self.play = function play(x, y, player) {
 		return new Promise((resolve, reject) => {
 
 			if (!self.isReady())
-				reject({error: 'Game not ready'})
+				reject({error: 'Game not ready'});
 			else if (x < 0 || x > 18)
-				reject({error: 'Bad position'})
+				reject({error: 'Bad position'});
 			else if (y < 0 || y > 18)
-				reject({error: 'Bad position'})
+				reject({error: 'Bad position'});
 			else if (player !== 1 && player !== 2)
-				reject({error: 'Bad player'})
+				reject({error: 'Bad player'});
 			else if (self.map[y][x] !== 0)
-				reject({error: 'There already is a stone there'})
+				reject({error: 'There already is a stone there'});
 			else {
-				self.map[y][x] = player
-				++self.turn
-				self.emit('turn', self.getTurn())
-				resolve(self.checkWin(x, y, player))
+				self.map[y][x] = player;
+				++self.turn;
+				self.emit('turn', self.getTurn());
+				resolve(self.checkWin(x, y, player));
 			}
 		})
-	}
+	};
 
 	self.checkValidMove = function () {
 		return true;
@@ -108,4 +108,4 @@ function Game () {
 
 util.inherits(Game, EventEmitter);
 
-module.exports = Game
+module.exports = Game;
